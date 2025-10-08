4390b7b4d5201617b8246f0b708bda5a057343ea
Lenny6path <lenny.mallot@edu.esiee-it.fr>
2025-10-08 11:11:32 +0200

    Menu

--- app/src/main/java/com/example/flashcard/MainActivity.java
+++ app/src/main/java/com/example/flashcard/MainActivity.java
@@ -1,29 +1,36 @@
 package com.example.flashcard;
 
 import android.os.Bundle;
-import android.util.Log;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.Button;
+import android.widget.Toast;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String TAG = "MainActivity";
+    private Button startQuizBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
-        Log.d(TAG, "Hello Flashcard");
+        startQuizBtn = findViewById(R.id.startQuizBtn);
+
+        startQuizBtn.setOnClickListener(v -> showDifficultyDialog());
+    }
+
+    private void showDifficultyDialog() {
+        final String[] levels = {"Facile", "Moyen", "Difficile"};
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Choisis ton niveau de difficulté");
+        builder.setItems(levels, (dialog, which) -> {
+            String selected = levels[which];
+            Toast.makeText(this, "Niveau choisi : " + selected, Toast.LENGTH_SHORT).show();
+            //  Ici je pourrais ensuite lancer mon QuizActivity selon le niveau choisi
+        });
+        builder.setNegativeButton("Annuler", (dialog, which) -> dialog.dismiss());
+        builder.show();
     }
 }
